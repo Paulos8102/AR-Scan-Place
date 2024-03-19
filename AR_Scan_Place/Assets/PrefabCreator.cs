c0bf6998dbf9c39a006677900a75e7153659d191
play <88676086+Paulos8102@users.noreply.github.com>
2024-03-19 18:23:12 +0530

    Dragon Animate

---
+++ AR_Scan_Place/Assets/PrefabCreator.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class PrefabCreator : MonoBehaviour
+{
+    [SerializeField] private GameObject dragonPrefab;
+    [SerializeField] private Vector3 prefabOffset;
+
+    private GameObject dragon;
+    private ARTrackedImageManager aRTrackedImageManager;
+
+    private void OnEnable()
+    {
+        aRTrackedImageManager = gameObject.GetComponent<ARTrackedImageManager>();
+        //subscribe to it's event
+        aRTrackedImageManager.trackedImagesChanged += OnImageChanged;
+    }
+
+    private void OnImageChanged(ARTrackedImagesChangedEventArgs obj)
+    {
+        foreach (ARTrackedImage image in obj.added)
+        {
+            dragon = Instantiate(dragonPrefab, image.transform);
+            dragon.transform.position += prefabOffset;
+        }
+    }
+}
