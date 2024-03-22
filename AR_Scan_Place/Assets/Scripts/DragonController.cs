0b83951e70c2b1fa686278481b515d89b85f2386
play <88676086+Paulos8102@users.noreply.github.com>
2024-03-22 17:33:07 +0530

    Script and controller update

--- AR_Scan_Place/Assets/Scripts/DragonController.cs
+++ AR_Scan_Place/Assets/Scripts/DragonController.cs
@@ -9,10 +9,13 @@ public class DragonController : MonoBehaviour
     private FixedJoystick fixedJoystick;
     private Rigidbody rigidBody;
 
+    private Animator dragonAnimator;
+
     private void OnEnable()
     {
         fixedJoystick = FindAnyObjectByType<FixedJoystick>();
         rigidBody = gameObject.GetComponent<Rigidbody>();
+        dragonAnimator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
@@ -24,6 +27,13 @@ public class DragonController : MonoBehaviour
         rigidBody.velocity = movement * speed;
 
         if (xVal != 0 && yVal != 0)
+        {
+            dragonAnimator.SetBool("isFlying", true);
             transform.eulerAngles = new Vector3(transform.eulerAngles.x, Mathf.Atan2(xVal, yVal) * Mathf.Rad2Deg, transform.eulerAngles.z);
+        }
+        else
+        {
+            dragonAnimator.SetBool("isFlying", false);
+        }
     }
 }
